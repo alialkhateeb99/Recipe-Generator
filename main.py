6683715b85db3d21865f7138311cd9224cb12861
Ali Alkhateeb <drastic67@gmail.com>
2020-09-19 18:05:49 +0000

    integrated OAuth authorization to access twitter API

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import flask
+import tweepy
 import os
 import random
+from tweepy import Cursor
+
+
+twitter_consumer_key = os.environ['KEY']
+twitter_consumer_secret = os.environ['KEY_SECRET']
+
+twitter_access_token = os.environ['TOKEN']
+twitter_access_token_secret = os.environ['TOKEN_SECRET']
+
+
+auth = tweepy.OAuthHandler(twitter_consumer_key,twitter_consumer_secret)
+auth.set_access_token(twitter_access_token,twitter_access_token_secret)
+
+api = tweepy.API(auth)
+
+
 
 app = flask.Flask(__name__)
 
@@ -10,7 +27,7 @@ def index():
     return flask.render_template("index.html")
     
 app.run(
-    debug= True,
+     debug= True,
       port=int(os.getenv('PORT',8080)),
       host= os.getenv('IP','0.0.0.0')
     )
