754975165337fccdca9e5a99b61876fbdfe1208f
Ali Alkhateeb <drastic67@gmail.com>
2020-09-22 23:33:13 +0000

    Tweets are now displayed with full text

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from tweepy import Cursor
 
 twitter_consumer_key = os.environ['KEY']
 twitter_consumer_secret = os.environ['KEY_SECRET']
-
 twitter_access_token = os.environ['TOKEN']
 twitter_access_token_secret = os.environ['TOKEN_SECRET']
 
@@ -15,22 +14,19 @@ auth = tweepy.OAuthHandler(twitter_consumer_key,twitter_consumer_secret)
 auth.set_access_token(twitter_access_token,twitter_access_token_secret)
 
 api = tweepy.API(auth)
-
-
 app = flask.Flask(__name__)
 
 food = ["ice cream","pizza","sushi","ramen","tacos","cheeseburger","mac and cheese","fried chicken","hamburger"]
 
-
 tweetcontent = ""
-tweetauthor  = "hey"
+tweetauthor  = ""
 tweetdate    = ""
     
 
 @app.route('/')
 def index():
     randomfoodname= random.choice(food)
-    tweets = api.search(q=randomfoodname,lang="en",count=5)
+    tweets = api.search(q=randomfoodname,lang="en",count=5,tweet_mode='extended')
     tweets_list = []
     
     for tweet in tweets:
@@ -38,7 +34,7 @@ def index():
 
     tweet2 = random.choice(tweets_list)
 
-    tweetcontent = tweet2.text
+    tweetcontent = tweet2.full_text
     tweetdate = tweet2.created_at
     tweetauthor = tweet2.user.screen_name
     
