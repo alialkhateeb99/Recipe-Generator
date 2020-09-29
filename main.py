b8914677d53ca1706fcf759e8e04b300fe7ca297
Ali Alkhateeb <drastic67@gmail.com>
2020-09-29 05:12:28 +0000

    Added spoonacular API service functionality to retrieve all data related to a food

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ import json
 dotenv_path = join(dirname(__file__), 'spoonacularkeys.env')
 load_dotenv(dotenv_path)
 
+dotenv_path2 = join(dirname(__file__), 'twitterkeys.env')
+load_dotenv(dotenv_path2)
+
 twitter_consumer_key = os.environ['KEY']
 twitter_consumer_secret = os.environ['KEY_SECRET']
 twitter_access_token = os.environ['TOKEN']
 twitter_access_token_secret = os.environ['TOKEN_SECRET']
 
 spoonacular_key = os.environ['SPOONACULAR_KEY']
+
+
 
 auth = tweepy.OAuthHandler(twitter_consumer_key,twitter_consumer_secret)
 auth.set_access_token(twitter_access_token,twitter_access_token_secret)
@@ -33,7 +38,30 @@ tweetdate    = ""
 
 @app.route('/')
 def index():
-    randomfoodname= random.choice(food)
+    randomfoodname = random.choice(food)
+    fetch_id_url   = "https://api.spoonacular.com/recipes/complexSearch?query={}&number={}&apiKey={}".format(randomfoodname,3,spoonacular_key)
+    
+    response  = requests.get(fetch_id_url)
+    json_body = response.json()
+    
+    id_to_fetch = json_body["results"][0]["id"]
+    
+    fetch_food_information_url = "https://api.spoonacular.com/recipes/{}/information?&apiKey={}".format(id_to_fetch,spoonacular_key)
+    
+    response2  = requests.get(fetch_food_information_url)
+    json_body2 = response2.json()
+    
+    food_title      = json_body2["title"]
+    food_servings   = json_body2["servings"]
+    food_prep_time  = json_body2["readyInMinutes"]
+    food_image_url  = json_body2["image"]
+    food_source_url = json_body2["sourceUrl"]
+
+    ingredients = []
+    for x in json_body2["extendedIngredients"]:
+        ingredients.append(x["originalString"])
+    ingredients_length = len(ingredients)
+    
     tweets = api.search(q=randomfoodname,lang="en",count=5,tweet_mode='extended')
     tweets_list = []
     
@@ -50,7 +78,15 @@ def index():
     foodname=randomfoodname,
     tweet_content=tweetcontent,
     tweet_date=tweetdate,
-    tweet_author=tweetauthor)
+    tweet_author=tweetauthor,
+    foodTitle=food_title,
+    foodServings=food_servings,
+    foodPrepTime=food_prep_time,
+    foodImageUrl=food_image_url,
+    foodSourceUrl=food_source_url,
+    foodIngredients=ingredients,
+    foodIngredientsLength=ingredients_length
+    )
     
 app.run(
      debug= True,
